5df818eba44325964b0755e4648846ff15b5512b
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2017-05-25 13:57:19 -0300

    Script send to NPAD

--- Class_assignments/sendFile2NPAD.sh
+++ Class_assignments/sendFile2NPAD.sh
@@ -5,7 +5,7 @@ then
   echo "When you wanto to send <MyFILE> to the <USERNAME> in the super computer, do the following:"
   echo "./send2NPAD.sh <MyFILE> <USERNAME>"
 else
-  scp -r -P4422 "${PWD}"/"$1" msdrjunior@sc.npad.imd.ufrn.br:~/
+  scp -r -P4422 "${PWD}"/"$1" "$2"@sc.npad.imd.ufrn.br:~/
 fi
 
 exit 0
